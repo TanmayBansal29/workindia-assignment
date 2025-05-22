c590c4d6388c931ccbacda7f374bad31212f4931
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-05-23 01:50:19 +0530

    Created route for specific booking details

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const { auth, isUser } = require("../middleware/auth")
-const { bookSeat } = require("../controllers/bookingController")
+const { bookSeat, getBookingDetails } = require("../controllers/bookingController")
 const router = express.Router()
 
 
 // Route for booking the train seat by user
 router.post("/book/:trainID", auth, isUser, bookSeat)
+
+// Route for getting specific booking details
+router.get("/booking/details/:bookingID", auth, isUser, getBookingDetails)
 
 module.exports = router
