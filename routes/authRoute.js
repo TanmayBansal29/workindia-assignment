73c4b81f6107c7eb99bd966c28022b3d80eb61de
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-05-22 22:58:13 +0530

    Created Route for user login

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,11 @@
 const express = require("express")
-const { registerUser } = require("../controllers/authController")
+const { registerUser, loginUser } = require("../controllers/authController")
 const router = express.Router()
 
 // Route for user registration
 router.get("/register", registerUser)
 
+// Route for user login
+router.get("/login", loginUser)
 
 module.exports = router
