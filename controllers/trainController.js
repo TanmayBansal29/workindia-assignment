c35c51791c824ae9d0708ac34ccc57f30919f3bd
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-05-23 00:10:31 +0530

    Created Controller for getting trains from source to destination

--- controllers/trainController.js
+++ controllers/trainController.js
@@ -52,3 +52,44 @@ exports.addTrain = async (req, res) => {
         })
     }
 }
+
+// Controller to get the train from source to destination
+exports.getTrains = async(req, res) => {
+    try {
+        const {source, destination} = req.body
+        if(!source || !destination) {
+            return res.status(400).json({
+                success: false,
+                message: "Please Enter all the fields"
+            })
+        }
+
+        const [trains] = await new Promise((resolve, reject) => {
+            db.query("SELECT * FROM trains WHERE source = ? AND destination = ?", [source, destination], (err, result) => {
+                if(err) {
+                    return reject(err)
+                }
+                resolve(result)
+            })
+        })
+
+        if(trains.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "No train found for specified source and destination"
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Trains fetched successfully",
+            data: trains
+        })
+    } catch (error) {
+        console.log("Error while fetching the trains data: ", error)
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong fetching the trains data"
+        })
+    }
+}
