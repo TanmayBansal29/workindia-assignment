1273aae95b8796d4a7987f7a6074e37ff5b6d9c9
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-05-22 22:31:19 +0530

    Registered route with express instance

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const db = require("./config/db")
 require("dotenv").config()
+const authRoutes = require("./routes/authRoute")
 const app = express()
 app.use(express.json())
 
@@ -10,10 +11,12 @@ app.listen(PORT, () => {
     console.log("Server is Running on Port: ", PORT)
 })
 
+app.use("/api/v1/user", authRoutes)
+
 app.get("/", (req, res) => {
     db.query('SELECT NOW()', (err, result) => {
         if(err) {
-            res.status(500).json({
+            return res.status(500).json({
                 success: false,
                 message: "Database Error"
             })
